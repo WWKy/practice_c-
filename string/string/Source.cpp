ebd41bf40cbb208999420ef0c0795e374e4436c5
wwk <285918693@qq.com>
2020-03-22 21:02:03 +0800

    update String.h Source.cpp

--- string/string/Source.cpp
+++ string/string/Source.cpp
@@ -1,92 +1,174 @@
-#include"String.h"
 #include<iostream>
+#include<assert.h>
+#include"String.h"
+
+using std::cout;
+using std::endl;
 
 void testConstructor();
+void testEqual();
 void testCopyConstructor();
 void testAssigment();
+void testPlus();
+void testSelfPlus();
+void testMultiple();
+//void tesSelftMultiple();
 
-int main() 
+#define DEFINE_STRINGS String s1(nullptr);String s2("");String s3;String s4("a");String s5("hello");
+
+int main()
 {
-	//testConstructor();
-	//testCopyConstructor();
+	testConstructor();
+	testEqual();
+	testCopyConstructor();
 	testAssigment();
+	testPlus();
+	testSelfPlus();
+	testMultiple();
 
 	system("pause");
 }
 
+// 单元测试
+// 1.正确情况
+// 2.边界值
+// 3.错误情况
 
-void testConstructor() 
+void testConstructor()
 {
-	std::cout << "构造函数测试**********" << std::endl;
-	String s1(nullptr);
-	String s2("");
-	String s3("a");
-	String s4("abcdefg");
-	s1.print();
-	s2.print();
-	s3.print();
-	s4.print();
-	std::cout << s1.length() << std::endl;
-	std::cout << s2.length() << std::endl;
-	std::cout << s3.length() << std::endl;
-	std::cout << s4.length() << std::endl;
-	std::cout << std::endl;
+	cout << "testConstructor\t";
+	DEFINE_STRINGS;
 
+	assert(strcmp(s1.getData(), "") == 0);
+	assert(strcmp(s2.getData(), "") == 0);
+	assert(strcmp(s3.getData(), "") == 0);
+	assert(strcmp(s4.getData(), "a") == 0);
+	assert(strcmp(s5.getData(), "hello") == 0);
+	assert(strcmp(s5.getData(), "Hello") != 0);
+
+	cout << "Pass!" << endl;
 }
 
-void testCopyConstructor() 
+void testEqual()
 {
-	std::cout << "拷贝构造函数测试**********" << std::endl;
-	String s1(nullptr);
-	String s2("");
-	String s3("a");
-	String s4("abcdefg");
+	cout << "testEqual\t";
+	DEFINE_STRINGS;
 
-	String s5(s1);
-	String s6(s2);
-	String s7(s3);
-	String s8(s4);
-	s5.print();
-	s6.print();
-	s7.print();
-	s8.print();
-	std::cout << s5.length() << std::endl;
-	std::cout << s6.length() << std::endl;
-	std::cout << s7.length() << std::endl;
-	std::cout << s8.length() << std::endl;
-	std::cout << std::endl;
+	assert(s1 == s1);
+	assert(s2 == s2);
+	assert(s3 == s3);
+	assert(s1 == s2);
+	assert(s2 == s3);
+
+	assert(s4 == s4);
+	assert(s5 == s5);
+
+	assert(s5 == s1 == false);
+	assert(s5 == s4 == false);
+
+	cout << "Pass!" << endl;
 }
 
+
+void testCopyConstructor()
+{
+	cout << "testCopyConstructor\t";
+	DEFINE_STRINGS;
+
+	String s_1(s1);
+	String s_2(s2);
+	String s_3(s3);
+	String s_4(s4);
+	String s_5(s5);
+
+	assert(s_1 == s1);
+	assert(s_2 == s2);
+	assert(s_3 == s3);
+	assert(s_4 == s4);
+	assert(s_5 == s5);
+	assert(s_1 != s_5);
+
+	cout << "Pass!" << endl;
+}
 
 void testAssigment()
 {
-	std::cout << "拷贝赋值函数测试**********" << std::endl;
-	String s1(nullptr);
-	String s2("");
-	String s3("a");
-	String s4("abcdefg");
-	String s5;
-	
-	s5 = s1;
-	s5.print();
-	std::cout << s5.length() << std::endl;
+	cout << "testAssigment\t";
+	DEFINE_STRINGS;
 
-	s5 = s2;
-	s5.print();
-	std::cout << s5.length() << std::endl;
-
-	s5 = s3;
-	s5.print();
-	std::cout << s5.length() << std::endl;
-
-	s5 = s4;
-	s5.print();
-	std::cout << s5.length() << std::endl;
-
+	s1 = s5;
+	s2 = s5;
+	s3 = s5;
+	s4 = s4;
 	s5 = s5;
-	s5.print();
-	std::cout << s5.length() << std::endl;
 
-	std::cout << std::endl;
+	assert(s1 == s5);
+	assert(s2 == s5);
+	assert(s3 == s5);
+	assert(s4 != s5);
+	assert(s5 == s5);
 
+	cout << "Pass!" << endl;
+}
+
+void testPlus()
+{
+	cout << "testPlus\t";
+	DEFINE_STRINGS;
+
+	assert(s1 + s2 == String(""));
+	assert(s1 + s4 == String("a"));
+	assert((s4 + s4) == String("aa"));
+	assert((s5 + s5) == String("hellohello"));
+	assert(String(" ") + String(" ") == String("  "));
+	assert(String(" ") + String(" ") != String(" "));
+
+	assert(s1 + s2 == (""));
+	assert(s1 + s4 == ("a"));
+	assert((s4 + s4) == ("aa"));
+	assert((s5 + s5) == ("hellohello"));
+	assert(String(" ") + String(" ") == ("  "));
+	assert(String(" ") + String(" ") != (" "));
+
+	cout << "Pass!" << endl;
+}
+
+void testSelfPlus()
+{
+	cout << "testSelfPlus\t";
+	DEFINE_STRINGS;
+
+	s1 += s1;
+	s2 += s2;
+	s3 += s3;
+	s4 += s4;
+	s5 += "abc";
+
+	assert(s1 == "");
+	assert(s2 == "");
+	assert(s3 == "");
+	assert(s4 == "aa");
+	assert(s5 == "helloabc");
+
+	cout << "Pass!" << endl;
+}
+
+void testMultiple()
+{
+	cout << "testMultiple\t";
+	DEFINE_STRINGS;
+
+	assert(s1 * 0 == "");
+	assert(s1 * 1 == s1);
+	assert(s1 * 2 == s1);
+	assert(s4 * 0 == "");
+	assert(s4 * 1 == "a");
+	assert(s4 * 2 == "aa");
+	assert(s5 * 0 == "");
+	assert(s5 * 1 == "hello");
+	assert(s5 * 2 == "hellohello");
+	assert(s5 * 3 == "hellohellohello");
+	assert(String(" ") * 2 == "  ");
+
+	cout << "Pass!" << endl;
 }
--- string/string/String.h
+++ string/string/String.h
@@ -3,17 +3,36 @@
 #include <cstring>
 #include <iostream>
 
-// strcpy_s不能拷贝0吗？怎么用
+using std::cout;
+using std::endl;
+using std::ostream;
+
+class String;
+ostream& operator<<(ostream&, const String&);
+
 class String
 {
 public:
-	String(const char* = nullptr); // 注意到空指针和无参数的处理方式一样
+	String(const char* = nullptr);	// 注意到空指针和无参数的处理方式一样
 	String(const String&);
+	String& operator=(const char*);
 	String& operator=(const String&);
-	~String();
-	void print() const;
+	~String() { delete[] data; };
+	bool operator==(const char*) const;
+	bool operator!=(const char*) const;
+	bool operator==(const String&) const;
+	bool operator!=(const String&) const;
+	String operator+(const char*) const;
+	String operator+(const String&) const;
+	String& operator+=(const char*);
+	String& operator+=(const String&);
+	String operator*(int) const;
+	String& operator*=(int);
 	int length() const { return strlen(data); };
+	char* getData() const { return data; };
 private:
+	static char* plusCore(const char*, const char*);
+	static char* mutipleCore(const char*, int);
 	char* data;
 };
 
@@ -27,33 +46,122 @@ inline String::String(const char* c)
 	else
 	{
 		data = new char[strlen(c) + 1]; // 预留给'\0'
-		strcpy(data, c);	// strcpy会复制'\0'
+		strcpy(data, c);				// strcpy会复制'\0'
 	}
 }
 
-String::String(const String& s)
+inline String::String(const String& s)
 {
 	data = new char[s.length() + 1];
 	strcpy(data, s.data);
 }
 
-String& String::operator=(const String& s)
+inline String& String::operator=(const char* c)
 {
-	if (this == &s)	// 防止傻傻的自赋值操作
-		return *this;
-
 	delete[] data;
-	data = new char[s.length() + 1];
-	strcpy(data, s.data);
+	data = new char[strlen(c) + 1];
+	strcpy(data, c);
 	return *this;
 }
 
-String::~String()
+inline String& String::operator=(const String& s)
 {
-	delete[] data;
+	if (this == &s)				// 防止傻傻的自赋值操作
+		return *this;
+	return operator=(s.data);
 }
 
-void String::print() const
+inline bool String::operator==(const char* r) const
 {
-	std::cout << data << std::endl;
+	return strcmp(data, r) == 0 ? true : false;
+}
+
+inline bool String::operator!=(const char* r) const
+{
+	return strcmp(data, r) != 0 ? true : false;
+}
+
+inline bool String::operator==(const String& r) const
+{
+	return operator==(r.data);
+}
+
+inline bool String::operator!=(const String & r) const
+{
+	return operator!=(r.data);
+}
+
+inline String String::operator+(const char* c) const
+{
+	return String(plusCore(data, c));
+}
+
+inline String String::operator+(const String& r) const
+{
+	return operator+(r.data);
+}
+
+inline String & String::operator+=(const char* r)
+{
+	char* new_c = plusCore(data, r);
+	delete[] data;
+	data = new_c;
+	return *this;
+}
+
+inline String& String::operator+=(const String& r)
+{
+	return operator+=(r.data);
+}
+
+inline String String::operator*(int n) const
+{
+	return String(mutipleCore(data, n));
+}
+
+inline String& String::operator*=(int n)
+{
+	char* new_c = mutipleCore(data, n);
+	delete[] data;
+	data = new_c;
+	return *this;
+}
+
+inline char* String::plusCore(const char* left, const char *right)
+{
+	char* new_c = new char[strlen(left) + strlen(right) + 1];
+	strcpy(new_c, left);
+	strcpy(new_c + strlen(left), right);
+	return new_c;
+}
+
+inline char* String::mutipleCore(const char* c, int n)
+{
+	char *new_c;
+	if (n == 0)
+	{
+		new_c = new char[1];
+		new_c[0] = '\0';
+	}
+	else if (n == 1)
+	{
+		new_c = new char[strlen(c)+1];
+		strcpy(new_c, c);
+	}
+	else
+	{
+		int len = strlen(c);
+		new_c = new char[len*n + 1];
+		for (int i = 0; i < n; i++) 
+		{
+			strcpy(new_c+i*len, c);
+		}
+	}
+	return new_c;
+}
+
+inline ostream& operator<<(ostream& os, const String& s)
+{
+	cout << s.getData();
+	return os;
 }
